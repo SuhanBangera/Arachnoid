758394b0c019594f31d97fa461017bde5b5838c0
Spyder01 <45194214+Spyder01@users.noreply.github.com>
2023-05-02 22:58:25 +0530

    Arachnoid 00 add readme spyder01 (#2)
    * ARACHNOID-01:Created Basic State Management System
    
    * ARACHNOID-00:ADDED NPM PUBLISH
    
    * FASTSCAN-00:Added actiion workflow
    
    ---------
    
    Co-authored-by: SJB <s.bangera@castsoftware.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/lib/index.ts',
+      name: 'arachnoid',
+      fileName: (format) => `arachnoid.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  }
+});
