12abec9cf12ef771bb015fc92acdfc9f2ac81feb
SJB <s.bangera@castsoftware.com>
2023-05-04 13:56:29 +0530

    ARACHNOID-00:Clean up

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 export {default as createStore} from './createStore';
-export {default as ArachnoidProvider} from './provider';
 export {default as createArachnoidMiddleware} from './createMiddleware';
 
 export type {Setter, Getter, Store, Action, Actions, ArachnoidMiddleware, Middleware} from './types';
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,26 +1,16 @@
 import type { ReactNode } from "react";
-import type { UseQueryOptions, Query, UseQueryResult } from "react-query";
 
 export type Getter<State> = () => State;
 export type StateAction<State> = (prevHook: State) => State;
 export type Setter<State> = (newState: State | StateAction<State>) => void;
 export type Action<State> = (get: Getter<State>, set: Setter<State>, payload?: any) => any;
 export type Actions<State> = Record<string, Action<State>>
-export type AsyncAction<State> = (get: Getter<State>, set: Setter<State>, response?: UseQueryResult) => any;
-export type AsyncActions<State> = Record<string, AsyncAction<State>>
 export type AsyncActionCallback<State> = (get: Getter<State>, set: Setter<State>, response: Response) => void;
 export type Middleware = <State>(get:Getter<State>, set:Setter<State>, middlewareAction:MiddlewareAction)=>void; 
 export type ArachnoidMiddleware = {
   middleware: Middleware, 
   ignore: string[]|undefined,
 }
-
-
-
-
-export type SpyAsyncActionQueryOptions<T> = Omit<UseQueryOptions<T, unknown, T, string[]>, "queryKey" | "queryFn"> & {
-  refetchInterval?: number | false | ((data: T | undefined, query: Query<T, unknown, T, string[]>) => number | false);
-};
 
 export interface MiddlewareAction {
   name: string
@@ -30,7 +20,6 @@ export interface MiddlewareAction {
 export interface Store<State> {
   state: State
   actions?: Actions<State>
-  asyncActions?: AsyncActions<State>
 }
 
 export interface SpyStoreProviderProps {
